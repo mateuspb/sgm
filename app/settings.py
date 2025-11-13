8cd2cb6af4183dfef37cb87ee8f3564ba34f125c
Mateus Perin Bastos <mateusperin@gmail.com>
2025-11-13 07:38:41 -0300

    config uwsgi params and static env root config

--- app/settings.py
+++ app/settings.py
@@ -149,6 +149,8 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
